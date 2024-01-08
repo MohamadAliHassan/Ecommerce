66bef2dce6bb7968bfd332980dbffd691c7ee77b
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2024-01-08 19:32:13 +0100

    working on header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import "./App.css";
 import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components/Layouts/Header";
+import { Footer } from "./components/Layouts/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <AllRoutes />
+      <Footer />
     </div>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,3 @@
 //Central export file for all our component files in the component folder.
+export { Header } from "./Layouts/Header";
+export { Footer } from "./Layouts/Footer";
