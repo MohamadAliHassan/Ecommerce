3b0394200b51dd9fae4c7cfd0eb4c6d431635cd6
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2024-04-02 14:59:26 +0200

    worked on fetching products from my json file to my components

--- src/pages/Products/ProductsList.js
+++ src/pages/Products/ProductsList.js
@@ -1,13 +1,30 @@
+import { useState, useEffect } from "react";
+import { ProductCard } from "../../components/Elements/ProductCard";
+import { FilterBar } from "./components/FilterBar";
+
 export const ProductsList = () => {
+  const [show, setShow] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    async function fetchProduct() {
+      const response = await fetch("http://localhost:8000/products");
+      const data = await response.json();
+
+      setProducts(data);
+    }
+    fetchProduct();
+  }, []);
   return (
     <main>
       <section className="my-5">
         <div className="my-5 flex justify-between">
           <span className="text-2xl font-semibold dark:text-slate-100 mb-5">
-            All eBooks (15)
+            All eBooks ({products.length})
           </span>
           <span>
             <button
+              onClick={() => setShow(!show)}
               id="dropdownMenuIconButton"
               data-dropdown-toggle="dropdownDots"
               className="inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-gray-100 rounded-lg hover:bg-gray-200 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-700"
@@ -28,8 +45,13 @@ export const ProductsList = () => {
 
         <div className="flex flex-wrap justify-center lg:flex-row">
           {/* Product Card */}
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
         </div>
       </section>
+      {/* If this is true then display it is what && basically means. Pass setshow as a prop to be able to use it on close filter button */}
+      {show && <FilterBar setShow={setShow} />}
     </main>
   );
 };
