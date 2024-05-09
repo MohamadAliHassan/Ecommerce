9c99c4c1fe40473980c59aa0d794d4f3e0b4ade6
MohamadAliHassan <113009645+MohamadAliHassan@users.noreply.github.com>
2024-05-09 12:21:25 +0200

    intergrated scroll to top functionality

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { Footer } from "./Layouts/Footer";
 export { ProductCard } from "./Elements/ProductCard";
 export { ProductDetail } from "../pages/ProductDetail";
 export { Rating } from "./Elements/Rating";
+export { ScrollToTop } from "./Other/ScrollToTop";
